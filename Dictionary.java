75b43e333830c86a6013d3c4d233577bc8371be6
Sameer Jorapur <sameervj@pacbell.net>
2017-12-27 09:59:39 -0800

    Add comments

--- Dictionary.java
+++ Dictionary.java
@@ -6,7 +6,7 @@ class Dictionary {
   
     Dictionary() {
       if (root == null) {
-        root = new TrieNode('^');
+        root = new TrieNode('^'); //Root is denoted as ^ node
       }
     }
   
@@ -14,14 +14,12 @@ class Dictionary {
 
     class TrieNode {
       
-      char c;
+      char c; //The char  
+      HashMap<String, TrieNode> childNodeMap = null; //Child map of all contained letters     
+      boolean isCompleteWord = false; //If its a intermediate word..for e.g if you insert apple and app, app is a internmediate word so set isComplete to true     
+      String meaning = null; //Stores the meaning
       
-      HashMap<String, TrieNode> childNodeMap = null;
-      
-      boolean isCompleteWord = false;
-      
-      String meaning = null;
-      
+      //Constructor
       TrieNode(char ch) {
         c = ch;
         childNodeMap = new HashMap<String, Dictionary.TrieNode>();
@@ -31,26 +29,36 @@ class Dictionary {
     }
   
     
+    /** insert() - Stores the word in the tree
+     * @param word
+     * @param meaning
+     * 
+     * We start with root and navigate each char in the word. If char is not found, we insert it in parents node. At the end we set isComplete = true and 
+     * set its meaning.
+     */
     public void insert(String word, String meaning) {
       
       //Start from top     
       char[] wordCharArray = word.toCharArray();
       TrieNode currentNode = root;
       TrieNode parentNode = null;
+      
       for (int i = 0; i < wordCharArray.length; i++) {
-        
         char currentChar = wordCharArray[i];
         if (currentNode.childNodeMap.get(""+currentChar) == null) {
-          //Create new
+          //Create new node
           TrieNode node = new TrieNode(currentChar);
-          //Attach to current
+          //Attach to current node
           currentNode.childNodeMap.put(""+currentChar, node);
         }
+        //Set currentNode as parent
         parentNode = currentNode;
         currentNode = parentNode.childNodeMap.get(""+currentChar);   
         int g=1;
       }//for
+      
       if (currentNode != null) {
+    	//The currentNode at this point will be pointing to the inserted word. We store its meaning.
         currentNode.meaning = meaning;
         currentNode.isCompleteWord = true;
       }      
